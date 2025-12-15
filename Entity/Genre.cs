e6d519f23a912f9e88247e46e3f7e3c2939e3d6d
ARIF AQEEL AHMAD <145203009+Arifaqeel05@users.noreply.github.com>
2025-12-15 12:50:40 +0500

    CORS implementation

---
+++ Entity/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Movie_App_MinimalApi.Entity
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,65 @@
+using Microsoft.AspNetCore.Cors;
+using Movie_App_MinimalApi.Entity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//service zone starts here
+
+
+//add cors service
+builder.Services.AddCors(myCorsSetting =>
+{
+    myCorsSetting.AddDefaultPolicy(permissions =>
+    {
+        //permissions is a parameter name, you can use any name
+        //fetching allowed origin from appsettings.Develoment.json-->builder.Configuration["allowedOrigin"]!
+        permissions
+                   .WithOrigins(builder.Configuration["allowedOrigin"]!)
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+    });
+
+    //add other policy-->we have to give policy name here it is "free"
+    //define it for the understanding purpose.only. as we have allowed origin in the default policy
+    /*myCorsSetting.AddPolicy("free", permissions =>
+    {
+             permissions
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+    });*/
+});
+
+//service zone ends here
+
 var app = builder.Build();
 
+
+//Middleware zone starts here
+
+app.UseCors(); //enable cors middleware with default policy.it will be applied globally 
+
+
+//in this endpoint,we are using other policy named "free".it will override the default policy
 app.MapGet("/", () => "Hello World!");
 
+
+
+//here ,we are using default policy which is defined in the middleware
+app.MapGet("/genre", () =>
+{
+    var genres = new List<Genre>()
+    {
+        new Genre() { Id = 1, Name = "Action" },
+        new Genre() { Id = 2, Name = "Comedy" },
+        new Genre() { Id = 3, Name = "Drama" }
+    };
+    return genres;
+});
+
+//Middleware zone ends here
+
 app.Run();
+
+
+//so in the development environment, we allowed all the origins to access our api
