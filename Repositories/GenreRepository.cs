2b6e5b32a7f3cd13b04a5c1168cfbcdaadf8c1ab
ARIF AQEEL AHMAD <145203009+Arifaqeel05@users.noreply.github.com>
2025-12-15 17:31:11 +0500

    feat: Dapper setup and connection to Database

---
+++ Repositories/GenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Movie_App_MinimalApi.Entity;
+
+namespace Movie_App_MinimalApi.Repositories
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly string? connectionString;
+
+        public GenreRepository(IConfiguration config)
+        {
+             connectionString = config.GetConnectionString("DefaultConnection")!;
+        }
+        public Task<int> Create(Genre genre)
+        {
+           
+        }
+    }
+}
---
+++ Repositories/IGenreRepository.cs
@@ -0,0 +1,10 @@
+﻿using Movie_App_MinimalApi.Entity;
+
+namespace Movie_App_MinimalApi.Repositories
+{
+    public interface IGenreRepository
+    {
+        //any class that implements this interface must implement this method
+        Task<int> Create(Genre genre);
+    }
+}
