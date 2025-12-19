0809d63675a00080d4a4688e2f8a75b31a64afb9
ARIF AQEEL AHMAD <145203009+Arifaqeel05@users.noreply.github.com>
2025-12-19 14:30:10 +0500

    fix:update and clean Endpoints

--- Program.cs
+++ Program.cs
@@ -67,7 +67,8 @@ app.UseOutputCache();
 
 //here ,we are using default policy which is defined in the middleware
 
-var genresEndpoint =app.MapGroup("/genres").WithTags("Genre Endpoints");//grouping the endpoints with common prefix /genres and adding tag for swagger documentation
+var genresEndpoint =app.MapGroup("/genres")
+    .WithTags("Genre Endpoints");//grouping the endpoints with common prefix /genres and adding tag for swagger documentation
 
 
 genresEndpoint.MapGet("/", GetAll)
@@ -75,41 +76,43 @@ genresEndpoint.MapGet("/", GetAll)
 //tag is used to evict or clear the cache when data is changed.
 
 
-
-
-genresEndpoint.MapGet("/{id:int}", async (int id, IGenreRepository genreRepository) =>
+genresEndpoint.MapGet("/{id:int}", GetById);
+/*async (int id, IGenreRepository genreRepository) =>
 {
-    var genre = await genreRepository.GetById(id);
-    if (genre is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(genre);
-});
-
-
-
-genresEndpoint.MapPost("/createGenre", async (Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
+var genre = await genreRepository.GetById(id);
+if (genre is null)
 {
-    await genreRepository.Create(genre);
-    await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
-    return Results.Created($"/genre/{genre.Id}",genre);
-});
+    return Results.NotFound();
+}
+return Results.Ok(genre);
+});*/
 
 
-genresEndpoint.MapPut("/updateGenre/{id:int}", async (int id,Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
+
+genresEndpoint.MapPost("/createGenre", Create);
+/*async (Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
 {
-    var existingGenre = await genreRepository.GetById(id);
-    if (existingGenre is null)
-    {
-        return Results.NotFound();
-    }
-    await genreRepository.Update(genre);//here we  call the update method of repository and pass the genre object
-    await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
-    return Results.NoContent();//204 no content because we are not returning any content
-});
+await genreRepository.Create(genre);
+await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
+return Results.Created($"/genre/{genre.Id}", genre);
+});*/
 
-genresEndpoint.MapDelete("/deleteGenre/{id:int}", async (int id, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
+
+genresEndpoint.MapPut("/updateGenre/{id:int}", Update);
+/* async (int id,Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
+{
+var existingGenre = await genreRepository.GetById(id);
+if (existingGenre is null)
+{
+    return Results.NotFound();
+}
+await genreRepository.Update(genre);//here we  call the update method of repository and pass the genre object
+await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
+return Results.NoContent();//204 no content because we are not returning any content
+});*/
+
+genresEndpoint.MapDelete("/deleteGenre/{id:int}", Delete);
+/* async (int id, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) =>
 {
     //var existingGenre = await genreRepository.GetById(id);
 
@@ -121,7 +124,7 @@ genresEndpoint.MapDelete("/deleteGenre/{id:int}", async (int id, IGenreRepositor
     await genreRepository.Delete(id);
     await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
     return Results.NoContent();
-});
+});*/
 
 
 
@@ -139,3 +142,50 @@ static async Task<Ok<List<Genre>>> GetAll (IGenreRepository genreRepository)
     var genres = await genreRepository.GetAll();
     return TypedResults.Ok(genres);
 }
+
+static async Task<Results<Ok<Genre>,NotFound>> GetById (int id, IGenreRepository genreRepository)
+
+//Task<Results<ok, not found>>--->it will return either ok or not found result.we mention the possible return types in the angle brackets because there are multiple return types.
+{
+    var genre = await genreRepository.GetById(id);
+    if (genre is null)
+    {
+        return TypedResults.NotFound();
+    }
+    return TypedResults.Ok(genre);
+}
+
+
+static async Task<Created<Genre>> Create(Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig)
+{
+    await genreRepository.Create(genre);
+    await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
+    return TypedResults.Created($"/genre/{genre.Id}", genre);
+}
+
+
+static async Task<Results<NoContent,NotFound>> Update(int id, Genre genre, IGenreRepository genreRepository, IOutputCacheStore cachecleanig)
+{
+    var existingGenre = await genreRepository.GetById(id);
+    if (existingGenre is null)
+    {
+        return TypedResults.NotFound();
+    }
+    await genreRepository.Update(genre);//here we  call the update method of repository and pass the genre object
+    await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
+    return TypedResults.NoContent();//204 no content because we are not returning any content
+}
+
+static async Task<Results<NotFound,NoContent>>Delete (int id, IGenreRepository genreRepository, IOutputCacheStore cachecleanig) 
+{
+    //var existingGenre = await genreRepository.GetById(id);
+
+    var existingGenre = await genreRepository.ExistGenre(id);
+    if (!existingGenre)
+    {
+        return TypedResults.NotFound();
+    }
+    await genreRepository.Delete(id);
+    await cachecleanig.EvictByTagAsync("genre-get", default);//evict the cache with tag "genre-get"
+    return TypedResults.NoContent();
+};
