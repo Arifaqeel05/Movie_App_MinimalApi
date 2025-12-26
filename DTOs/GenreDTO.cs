b2dc10dbbb34c8eef07dad4888e6a4d01b30a236
ARIF AQEEL AHMAD <145203009+Arifaqeel05@users.noreply.github.com>
2025-12-26 20:07:23 +0500

    Add GenreDTO for improved data encapsulation

---
+++ DTOs/GenreDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Movie_App_MinimalApi.DTOs
+{
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
+
+
+/*purpose of this dto is to return final response to the client
+without exposing any internal entity details eventhough in this case entity and dto are same*/
