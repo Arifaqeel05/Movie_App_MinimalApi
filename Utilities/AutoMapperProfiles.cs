46080b2423cbd93203d7cb663666bdfc2c2b9adf
ARIF AQEEL AHMAD <145203009+Arifaqeel05@users.noreply.github.com>
2025-12-27 02:33:48 +0500

    fix &feat: automapper implementaion and bug fixing

---
+++ Utilities/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Movie_App_MinimalApi.DTOs;
+using Movie_App_MinimalApi.Entity;
+
+namespace Movie_App_MinimalApi.Utilities
+{
+    public class AutoMapperProfiles:Profile
+    {
+        //constructor to put all the automapping configurations
+        public AutoMapperProfiles()
+        {
+            CreateMap<Genre, GenreDTO>();
+            CreateMap<CreateUpdateGenreDTO, Genre>();
+        }
+    }
+}
